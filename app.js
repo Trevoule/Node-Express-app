1639205c93c44309a3daa3946c7e63a83dc9a9c8
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2023-01-06 20:37:19 +0100

    3. Handling POST Requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello for server', app: 'Natours' });
 // });
@@ -19,6 +22,23 @@ app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   body received because of middleware
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
 });
 
 const port = '3000';
