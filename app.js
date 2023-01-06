cfa315c02cf29289c58228d28c79cf608f2843b0
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2023-01-06 20:55:05 +0100

    4. Responding to URL Parameter

--- app.js
+++ app.js
@@ -15,8 +15,20 @@ app.use(express.json());
 // });
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'invalid ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+});
 
 app.get('/api/v1/tours', (req, res) => {
   res
